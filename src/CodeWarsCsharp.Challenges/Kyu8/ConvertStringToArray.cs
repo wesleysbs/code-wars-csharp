c0a3ce9babedcc72483bcad22912581c55ed58bd
Wesley <wesleysbs@hotmail.com>
2026-02-23 20:44:26 -0300

    feat: add new projects with a sample exercise

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ConvertStringToArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public static class ConvertStringToArray
+{
+    public static string[] StringToArray(string str)
+    {
+        return str.Split(' ');
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ConvertStringToArrayTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ConvertStringToArrayTests
+{
+    [Fact] 
+    public void StringToArray_GivenAString_ShouldReturnExpectedArray()
+    {
+        //Arrange
+        var inputStringValue = "Wesley moto";
+        var expectedResult = new string[] { "Wesley", "moto" };
+
+        //Act
+        var result = ConvertStringToArray.StringToArray(inputStringValue);
+      
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }  
+}
