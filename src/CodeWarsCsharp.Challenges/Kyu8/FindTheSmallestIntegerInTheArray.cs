183f2429e66a70232525125a876d17bd6bb28e63
Wesley <wesleysbs@hotmail.com>
2026-02-25 21:47:24 -0300

    feat: add new exercises

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/FindTheSmallestIntegerInTheArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public static class FindTheSmallestIntegerInTheArray
+{
+    public static int FindSmallestInt(int[] args)
+    {
+        int smallestInt = args[0];
+
+        foreach (int i in args)
+        {
+            if (i<smallestInt)
+                smallestInt = i;
+}
+
+        return smallestInt;
+    }
+}
