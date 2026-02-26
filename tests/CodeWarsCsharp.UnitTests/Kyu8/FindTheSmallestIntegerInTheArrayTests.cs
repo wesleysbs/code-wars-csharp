5fe70dd47a0ee34c5c397def3de8670a14fe9f4c
Wesley <wesleysbs@hotmail.com>
2026-02-25 21:56:20 -0300

    feat: add new exercises (#2)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/FindTheSmallestIntegerInTheArrayTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class FindTheSmallestIntegerInTheArrayTests
+{
+    [Fact]
+    public void FindSmallestInt_GivenAArray_ShouldReturnExpectedSmallestInteger()
+    {
+        //Arrange
+        var inputIntValue = new int[] { 34, 15, 88, 2 };
+        var expectedResult = 2;
+
+        //Act
+        var result = FindTheSmallestIntegerInTheArray.FindSmallestInt(inputIntValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
