75703936ab64b35f135a2ceaa6cf8147f82510c6
Wesley <wesleysbs@hotmail.com>
2026-02-27 18:27:49 -0300

    feat: add GrasshopperArrayMean challenge and tests (#4)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/GrasshopperArrayMean.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class GrasshopperArrayMean
+{
+    public static int FindAverage(int[] marks)
+    {
+        int sum = 0;
+        foreach (int mark in marks)
+        {
+            sum += mark;
+        }
+        return sum / marks.Length;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/GrasshopperArrayMeanTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class GrasshopperArrayMeanTests
+{
+    [Fact]
+    public void GetMeanArray_GivenArrayOfIntegers_ShouldReturnCorrectMean()
+    {
+        //Arrange
+        var inputValue = new int[] { 1, 3, 5, 7 };
+        var expectedResult = 4;
+
+        //Act
+        var result = GrasshopperArrayMean.FindAverage(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
