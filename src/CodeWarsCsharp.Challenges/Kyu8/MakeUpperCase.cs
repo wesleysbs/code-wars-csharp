2d9adeca8e672ab283414f07f8917a8ec229ee0f
Wesley <wesleysbs@hotmail.com>
2026-02-27 23:00:24 -0300

    feat: add MakeUperCase challenge and tests (#5)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/MakeUpperCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class Kata
+{
+    public static string MakeUpperCase(string str)
+    {
+        return str.ToUpper();
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/MakeUpperCaseTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class MakeUpperCaseTests
+{
+    [Fact]
+
+    public void MakeUpperCase_GivenAString_ShouldReturnUpperCase()
+    {
+        //Arrange
+        var inputValue = "hello";
+        var expectedValue = "HELLO";
+
+        //Act
+        var result = Kata.MakeUpperCase(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
