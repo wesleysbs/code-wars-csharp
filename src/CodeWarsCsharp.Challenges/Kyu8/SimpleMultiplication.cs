c02965c39270d6de256d6a05d3b80ec5b598b673
Wesley <wesleysbs@hotmail.com>
2026-03-03 15:20:15 -0300

    feat: add SimpleMultiplication challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SimpleMultiplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SimpleMultiplication
+{  
+    public static int Multiply(int x)
+    {
+        if (x % 2 == 0)
+        {
+            return x * 8;
+        }
+
+        else
+        {
+            return x * 9;
+        }
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SimpleMultiplicationTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SimpleMultiplicationTests
+{
+    [Theory]
+    [InlineData(1, 9)]
+    [InlineData(2, 16)]
+
+    public void Multiply_GivenAnInteger_ShouldReturnMultipliedValue(int inputValue, int expectedValue)
+    {
+        //Act
+        var result = SimpleMultiplication.Multiply(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);       
+    }
+}
