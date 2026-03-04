ace79e62697be1c77026d5194a144b0c78ffb706
Wesley <wesleysbs@hotmail.com>
2026-03-04 20:02:20 -0300

    feat: add TheWideMouthedFrog challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/TheWideMouthedFrogTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class TheWideMouthedFrogTests
+{
+    [Theory]
+    [InlineData("alligator", "small")]
+    [InlineData("ALLIGATOR", "small")]
+    [InlineData("ant bear", "wide")]
+
+    public void MouthSize_GivenAnimal_ShouldReturnCorrectMouthSize(string inputValue, string expectedValue)
+    {
+        //Act
+        var result = TheWideMouthedFrog.MouthSize(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
