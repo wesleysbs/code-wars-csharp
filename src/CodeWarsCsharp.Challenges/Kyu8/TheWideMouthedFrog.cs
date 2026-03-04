7826f0586bbe8394dc48ecc2c0d7477571475e7a
Wesley <wesleysbs@hotmail.com>
2026-03-04 20:05:10 -0300

    feat: add TheWideMouthedFrog challenge and tests (#8)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/TheWideMouthedFrog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class TheWideMouthedFrog
+{
+    public static string MouthSize(string animal)
+    {
+        if (string.Equals(animal, "alligator", StringComparison.OrdinalIgnoreCase))
+        {
+            return "small";
+        }
+        else
+        {
+            return "wide";
+        }
+    }
+}
