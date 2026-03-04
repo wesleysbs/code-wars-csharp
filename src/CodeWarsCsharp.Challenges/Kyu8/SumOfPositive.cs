5afa82a781d7d4bb17f6d46c23ef689b11e06bb9
Wesley <wesleysbs@hotmail.com>
2026-03-04 19:29:15 -0300

    feat: add SumOfPositive challenge and tests (#7)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SumOfPositive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SumOfPositive
+{
+    public static int PositiveSum(int[] arr)
+    {
+        int total = 0;
+
+        if (arr == null) return 0;
+
+        foreach (int n in arr)
+        {
+            if (n > 0) total += n;
+        }
+        return total;
+    }
+}
