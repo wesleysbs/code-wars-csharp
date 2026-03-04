49234a34870a2408556f8ce2c723d736085c93f4
Wesley <wesleysbs@hotmail.com>
2026-03-04 20:29:41 -0300

    feat: add ThinkfulLogicDrillsTrafficLight challenge and tests (#9)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ThinkfulLogicDrillsTrafficLightTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ThinkfulLogicDrillsTrafficLightTests
+{
+    [Theory]
+    [InlineData("green", "yellow")]
+    [InlineData("yellow", "red")]
+    [InlineData("red", "green")]
+
+    public void UpdateLight_GivenCurrentColor_ShouldReturnNextColor(string inputValue, string expectedValue)
+    {
+        //Act
+        var result = ThinkfulLogicDrillsTrafficLight.UpdateLight(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
