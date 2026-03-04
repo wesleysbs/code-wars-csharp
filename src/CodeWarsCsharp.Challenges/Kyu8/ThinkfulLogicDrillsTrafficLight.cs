3ccf1add590812e22eb0fca7075106c216b265df
Wesley <wesleysbs@hotmail.com>
2026-03-04 20:28:27 -0300

    feat: add ThinkfulLogicDrillsTrafficLight challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ThinkfulLogicDrillsTrafficLight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ThinkfulLogicDrillsTrafficLight
+{
+    public static string UpdateLight(string current)
+    {
+        if (current == "green")
+        {
+            return "yellow";
+        }
+
+        else if (current == "yellow")
+        {
+            return "red";
+        }
+
+        else if (current == "red")
+        {
+            return "green";
+        }
+        return current;
+    }
+}
