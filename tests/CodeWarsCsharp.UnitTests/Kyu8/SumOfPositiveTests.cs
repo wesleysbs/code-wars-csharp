b68b75a0b7f37e14bb8a76599c95b46d1a0f06c3
Wesley <wesleysbs@hotmail.com>
2026-03-04 19:28:06 -0300

    feat: add SumOfPositive challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SumOfPositiveTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SumOfPositiveTests
+{
+    [Theory]
+    [InlineData(new int[]{1, 2, 3, 4, 5}, 15)]
+    [InlineData(null, 0)]
+    
+    public void SumOfPositive_GivenAnArray_ShouldExpectedPositiveSum(int[] inputValue, int expectedValue)
+    {
+        //Act
+        var result = SumOfPositive.PositiveSum(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
