2e93991a1909fa702a0234bdf75fb6112221beb2
Wesley <wesleysbs@hotmail.com>
2026-03-06 23:01:37 -0300

    feat: add VolumeOfACuboid challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/VolumeOfACuboid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class VolumeOfACuboid
+{
+    public static double GetVolumeOfCuboid(double length, double width, double height)
+    {
+        return length * width * height;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/VolumeOfACuboidTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class VolumeOfACuboidTests
+{
+    [Fact]
+
+    public void GetVolumeOfCuboid_GivenDimensions_ShouldReturnCorrectVolume()
+    {
+        //Arrange
+        const int length = 2;
+        const int width = 5;
+        const int height = 6;
+        
+        const int expectedValue = 60;
+
+        //Act
+        var result = VolumeOfACuboid.GetVolumeOfCuboid(length, width, height);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
