c3a92e1ff4ac3f17cdbde2f48c14265684666454
Wesley <wesleysbs@hotmail.com>
2026-03-07 19:31:38 -0300

    feat: add SumArrays challenge and tests (#12)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SumArraysTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SumArraysTests
+{
+    [Theory]
+    [InlineData(null, 0.0)]
+    [InlineData(new double[] { 1, 5.2, 4, 0, -1 }, 9.2)]
+
+    public void SumArray_GivenArray_ShouldReturnCorrectSum(double[]? inputValue, double expectedValue)
+    {
+        //Act
+        var result = SumArrays.SumArray(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
