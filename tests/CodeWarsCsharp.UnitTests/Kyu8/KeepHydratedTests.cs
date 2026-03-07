415c493cee431075f3316e02ca8e8d4ee6e04174
Wesley <wesleysbs@hotmail.com>
2026-03-06 23:31:50 -0300

    feat: add KeepHydrated challenge and tests (#11)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/KeepHydratedTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class KeepHydratedTests
+{
+    [Theory]
+    [InlineData(2.0,1)]
+    [InlineData(12.6, 6)]
+    [InlineData(0.82, 0)]
+    [InlineData(1787.0, 893)]
+    [InlineData(0.0, 0)]
+    public void Litres_GivenTime_ShouldReturnCorrectLitres(double inputValue, int expectedValue)
+    {
+        //Act
+        var result = KeepHydrated.Litres(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
