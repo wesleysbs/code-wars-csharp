04dc2c82c2f577e1479b040620e97904c62292b2
Wesley <wesleysbs@hotmail.com>
2026-03-06 23:30:37 -0300

    feat: add KeepHydrated challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/KeepHydrated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class KeepHydrated
+{
+    public static int Litres(double time)
+    {  
+        return (int) (time * 0.5);
+    }
+}
