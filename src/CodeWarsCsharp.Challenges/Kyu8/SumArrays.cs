ca8fcea38535a2be0e7ed41f872e95d37f963aab
Wesley <wesleysbs@hotmail.com>
2026-03-07 19:30:27 -0300

    feat: add SumArrays challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SumArrays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SumArrays
+{
+    public static double SumArray(double[] array)
+    {
+        if (array == null) return 0;
+        double sum = 0.0;
+        foreach (double v in array)
+        {
+            sum += v;
+        }
+        return sum;
+    }
+}
