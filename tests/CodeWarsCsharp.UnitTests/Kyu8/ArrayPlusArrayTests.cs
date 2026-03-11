af2ce802c1300fa6481446935dec5071cf679a5b
Wesley <wesleysbs@hotmail.com>
2026-03-11 20:52:47 -0300

    feat: add ArrayPlusArray challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ArrayPlusArrayTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ArrayPlusArrayTests
+{
+    [Fact]
+    public void SumArrays_GivenTwoArrays_ShouldReturnCorrectSum()
+    {
+        //Arrange
+        var firstInput = new int[] { 1, 2, 3 };
+        var secondInput = new int[] { 4, 5, 6 };
+
+        var expectedResult = 21;
+        
+        //Act
+        var result = ArrayPlusArray.SumArrays(firstInput, secondInput);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
