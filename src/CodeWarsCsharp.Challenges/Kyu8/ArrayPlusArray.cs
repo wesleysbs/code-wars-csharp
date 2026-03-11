922fe4517ec49b6ea0bc1fc4cbebe3566fbf5025
Wesley <wesleysbs@hotmail.com>
2026-03-11 20:54:05 -0300

    feat: add ArrayPlusArray challenge and tests (#13)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ArrayPlusArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ArrayPlusArray
+{
+    public static int SumArrays(int[] arr1, int[] arr2)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            sum += arr1[i];
+        }
+
+        for (int i = 0; i < arr2.Length; i++)
+        {
+            sum += arr2[i];
+        }
+        return sum;
+    }
+}
