311b65bbe39fcc0878024fbe949d2c08cd9bcf48
Wesley <wesleysbs@hotmail.com>
2026-03-11 21:22:01 -0300

    feat: add MakingSixToast challenge and tests (#15)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/MakingSixToast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class MakingSixToast
+{
+    public static int SixToast(int num)
+    {
+        return Math.Abs(num - 6);
+    }
+}
