4cbd25e90426223456c61de02294bbee03f46d90
Wesley <wesleysbs@hotmail.com>
2026-03-11 21:08:29 -0300

    feat: add ReduceButGrow challenge and tests (#14)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ReduceButGrow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ReduceButGrow
+{
+    public static int Grow(int[] x)
+    {
+        int grow = 1;
+        for (int i = 0; i < x.Length; i++)
+        {
+            grow = grow * x[i];
+        }
+        return grow;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ReduceButGrowTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ReduceButGrowTests
+{
+    [Fact]
+
+    public void ReduceButGrow_GivenAnArray_ShouldReturnCorrectProduct()
+    {
+        //Arrange
+        var inputValue = new int[] { 4, 1, 1, 1, 4 };
+        var expectedValue = 16;
+
+        //Act
+        var result = ReduceButGrow.Grow(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
