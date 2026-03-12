66941466fe370dec43031f578472cbefd6a91ea4
Wesley <wesleysbs@hotmail.com>
2026-03-11 21:20:34 -0300

    feat: add MakingSixToast challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/MakingSixToastTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class MakingSixToastTests
+{
+    [Fact]
+
+    public void MakingSixToast_GivenANumber_ShouldReturnToastDifference()
+    {
+        //Arrange
+        var inputValue = 6;
+        var expectedValue = 0;
+
+        //Act
+        var result = MakingSixToast.SixToast(inputValue);
+        
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
