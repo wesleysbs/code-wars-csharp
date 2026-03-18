2abb426fd2d4fc1dd5c06232281cfbc0708ce3a6
Wesley <wesleysbs@hotmail.com>
2026-03-18 11:27:43 -0300

    feat: add WillYouMakeIt challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/WillYouMakeIt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class WillYouMakeIt
+{
+    public static bool ZeroFuel(double distanceToPump, double mpg, double fuelLeft)
+    {
+        return mpg * fuelLeft >= distanceToPump;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/WillYouMakeItTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class WillYouMakeItTests
+{
+    [Theory]
+    [InlineData(50, 25, 2, true)]
+    [InlineData(100, 50, 1, false)]
+
+    public void ZeroFuel_GivenValidInputs_ShouldReturnTrueIfCarCanReachPump(double distanceToPump, double mpg, double fuelLeft, bool expected)
+    {
+        //Act
+        var result = WillYouMakeIt.ZeroFuel(distanceToPump, mpg, fuelLeft);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
