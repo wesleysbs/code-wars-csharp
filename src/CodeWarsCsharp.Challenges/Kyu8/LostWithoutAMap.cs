4b3a9d3bf68747dc73187762f1552730eae3a517
Wesley <wesleysbs@hotmail.com>
2026-03-18 10:51:10 -0300

    feat: add LostWithoutAMap challenge and tests (#16)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/LostWithoutAMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class LostWithoutAMap
+{
+        public static int[] Maps(int[] x)
+        {
+            int[] newArray = new int[x.Length];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                newArray[i] = x[i] * 2;
+            }
+            return newArray;
+        }
+    }
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/LostWithoutAMapTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class LostWithoutAMapTests
+{
+    [Fact]
+
+    public void Maps_GivenArray_ShouldReturnDoubledArray()
+    {
+        //Arrange
+        var inputValue = new int[] { 1, 2, 3 };
+        var expectedResult = new int[] { 2, 4, 6 };
+
+        //Act
+        var result = LostWithoutAMap.Maps(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
