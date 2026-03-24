bb7bfe9ebd287e44db5ae9c4b9b98fee4fd2cdaf
Wesley <wesleysbs@hotmail.com>
2026-03-24 15:09:14 -0300

    feat: add FindOutWheterTheShapeIsACube challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/FindOutWhetherTheShapeIsACube.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+    public class CubeChecker
+    {
+        public bool IsCube(double volume, double side)
+        {
+            if (volume <= 0 || side <= 0)
+            {
+                return false;
+            }
+            return volume == side * side * side;
+        }
+    }
+
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/FindOutWhetherTheShapeIsACubeTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class FindOutWhetherTheShapeIsACubeTests
+{
+    [Theory]
+    [InlineData(0.0, 0.0, false)]
+    [InlineData(8.0, 2.0, true)]
+    [InlineData(2.0, 1.0, false)]
+    
+    public void IsACube_GivenVolumeAndSide_ShouldReturnTrueOrFalse(double volume, double size, bool expected)
+    {
+        //Arrange
+        var cubeChecker = new CubeChecker();
+        
+        //Act
+        var result = cubeChecker.IsCube(volume, size);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
