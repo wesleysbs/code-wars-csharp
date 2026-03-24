b6169df3dcd6d3bec195ac127c5961018b853d4d
Wesley <wesleysbs@hotmail.com>
2026-03-24 16:04:24 -0300

    feat: add ThirAngleOfATriangle challenge and tests (#19)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ThirdAngleOfATriangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.ThirdAngle;
+
+public class Kata
+    {
+        public static int OtherAngle(int angle1, int angle2)
+        {
+            return 180 - (angle1 + angle2);
+        }
+    }
+
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ThirdAngleOfATriangleTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.ThirdAngle;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ThirdAngleOfATriangleTests
+{
+    [Theory]
+    [InlineData(30, 60, 90)]
+    [InlineData(60, 60, 60)]
+    [InlineData(43, 78, 59)]
+    [InlineData(10, 20, 150)]
+
+    public void OtherAngle_GivenTwoAngles_ShouldReturnThirdAngle(int angle1, int angle2, int expected)
+    {
+        //Act
+        var result = Kata.OtherAngle(angle1, angle2);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
