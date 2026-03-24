d9bb0c7463e8e9297a50d5effb035b1bd9e5d3f4
Wesley <wesleysbs@hotmail.com>
2026-03-24 17:50:10 -0300

    feat: add CenturyFromYear challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/CenturyFromYear.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.CenturyFromYear;
+
+public static class Kata
+{
+    public static int CenturyFromYear (int year)
+    {
+        return (year + 99) / 100;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/CenturyFromYearTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.CenturyFromYear;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class CenturyFromYearTests
+{
+    [Theory]
+    [InlineData(1705, 18)]
+    [InlineData(1900, 19)]
+    [InlineData(1601, 17)]
+    [InlineData(2000, 20)]
+    [InlineData(2020, 21)]
+
+    public void CenturyFromYear_GivenAnYear_ShoulReturnCorrectCentury(int year, int expected)
+    {
+        //Act
+        var result = Kata.CenturyFromYear(year);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
