8f9c331edea06a7ebb38cd2c1b893e8daa6af31e
Wesley <wesleysbs@hotmail.com>
2026-03-25 11:47:46 -0300

    feat: add ReturnTheDay challenge and tests (#22)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ReturnTheDayTests.cs
@@ -0,0 +1,31 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.ReturnTheDay;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ReturnTheDayTests
+{
+    [Theory]
+    [InlineData(1, "Sunday")]
+    [InlineData(2, "Monday")]
+    [InlineData(3, "Tuesday")]
+    [InlineData(4, "Wednesday")]
+    [InlineData(5, "Thursday")]
+    [InlineData(6, "Friday")]
+    [InlineData(7, "Saturday")]
+    [InlineData(8, "Wrong, please enter a number between 1 and 7")]
+    [InlineData(0, "Wrong, please enter a number between 1 and 7")]
+    [InlineData(-1, "Wrong, please enter a number between 1 and 7")]
+    [InlineData(20, "Wrong, please enter a number between 1 and 7")]
+
+    public void ReturnTheDay_GivenANumber_ShouldReturnCorrectDayOfTheWeek(int number, string expected)
+    {
+        //Act
+        var result = Kata.WhatDay(number);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
