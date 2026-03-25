f3f0467c017b5967c544c0f5418dbbd7b5f3f5bd
Wesley <wesleysbs@hotmail.com>
2026-03-25 11:30:44 -0300

    feat: add GrasshopperTerminalGameCombatFunction challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/GrasshopperTerminalGameCombatFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class Game
+{
+
+    public static float Combat(float health, float damage)
+    {
+        return Math.Max(0f, health - damage);
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/GrasshopperTerminalGameCombatFunctionTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class GrasshopperTerminalGameCombatFunctionTests
+{
+    [Theory]
+    [InlineData(100.0, 5.0, 95.0)]
+    [InlineData(92.0, 8.0, 84.0)]
+    [InlineData(20.0, 30.0, 0.0)]
+
+    public void GameCombat_GivenHealthAndDamage_ShouldReturnRemainingHealth(float health, float damage, float expected)
+    {
+        //Act
+        var result = Game.Combat(health, damage);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
