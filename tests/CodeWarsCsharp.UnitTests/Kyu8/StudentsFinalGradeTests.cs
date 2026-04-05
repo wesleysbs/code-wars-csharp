ceaab346fc6f281f0aaf001e9407c1b36ace08de
Wesley <wesleysbs@hotmail.com>
2026-04-05 10:52:39 -0300

    feat: add StudentsFinalGrade challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/StudentsFinalGradeTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.FinalGrade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class StudentsFinalGradeTests
+{
+    [Theory]
+    [InlineData(100, 12, 100)]
+    [InlineData(85, 5, 90)]
+
+    public void StudentsFinalGrade_GivenExamsAndProjects_ShouldReturnCorrectGrade(int exam, int projects, int expected)
+    {
+        //Act
+        var result = Kata.FinalGrade(exam, projects);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
