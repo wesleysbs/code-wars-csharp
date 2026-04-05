9d4dd36ef33c9f77d98c801cd83c252abf1345c7
Wesley <wesleysbs@hotmail.com>
2026-04-05 11:07:38 -0300

    feat: add TwiceAsOld challenge and tests (#30)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/TwiceAsOld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+using System;
+
+public class TwiceAsOldSolution
+{
+    public static int TwiceAsOld(int dadYears, int sonYears)
+    {
+        return Math.Abs(dadYears - (2 * sonYears));
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/TwiceAsOldTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class TwiceAsOldTests
+{
+    [Theory]
+    [InlineData(30, 0, 30)]
+    [InlineData(30, 7, 16)]
+    [InlineData(45, 30, 15)]
+
+    public void TwiceAsOld_GivenDadYearsAndSonYears_ShouldReturnYearsUntilTwiceAsOld(int dadYears, int sonYears, int expected)
+    {
+        //Act
+        var result = TwiceAsOldSolution.TwiceAsOld(dadYears, sonYears);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
