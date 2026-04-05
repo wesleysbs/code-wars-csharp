b52902c251cfbaf374fe8453cc9ccb6d07ab0708
Wesley <wesleysbs@hotmail.com>
2026-04-05 10:53:42 -0300

    feat: add StudentsFinalGrade challenge and tests (#29)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/StudentsFinalGrade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.FinalGrade;
+
+public class Kata
+{
+
+    public static int FinalGrade(int exam, int projects)
+    {
+        if (exam > 90 || projects > 10) return 100;
+        if (exam > 75 && projects >= 5) return 90;
+        if (exam > 50 && projects >= 2) return 75;
+        return 0;
+    }
+}
