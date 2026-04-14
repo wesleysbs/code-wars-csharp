7c483781e56aa3ead681dc8d4caf7cb4dfb3d13b
Wesley <wesleysbs@hotmail.com>
2026-04-14 10:47:48 -0300

    feat: add Quadrants challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/Quadrants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.Quadrants;
+
+public class Kata
+{
+    public static int Quadrant(int x, int y)
+    {
+        if (x > 0 && y > 0)
+        {
+            return 1;
+        }
+        else if (x < 0 && y > 0)
+        {
+            return 2;
+        }
+        else if (x < 0 && y < 0)
+        {
+            return 3;
+        }
+        else
+        {
+            return 4;
+        }
+    }
+}
