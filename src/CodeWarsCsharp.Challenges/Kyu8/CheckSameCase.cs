2d1975cd228c4417c08fec17a88f95cd72e663c3
Wesley <wesleysbs@hotmail.com>
2026-05-29 23:05:27 -0300

    feat: add CheckSameCase challenge and tests (#46)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/CheckSameCase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.SameCase;
+
+public class Kata
+{
+    public static int SameCase(char a, char b, int expected)
+    {
+        if (!char.IsLetter(a) || !char.IsLetter(b))
+            return -1;
+
+        if (char.IsUpper(a) == char.IsUpper(b))
+            return 1;
+
+        return 0;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/CheckSameCaseTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.SameCase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class CheckSameCaseTests
+{
+    [Theory]
+    [InlineData('a', 'u', 1)]
+    [InlineData('a', 'U', 0)]
+    [InlineData('a', '*', -1)]
+
+    public void CheckSameCase_GivenTwoCharacters_ShouldReturnExpectedResult(char a, char b, int expected)
+    {
+        //Act
+        var result = Kata.SameCase(a, b, expected);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
