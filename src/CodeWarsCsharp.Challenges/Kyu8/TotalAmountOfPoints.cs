c07bc306297e5f84c66c5cab4a3dfb382058db05
Wesley <wesleysbs@hotmail.com>
2026-06-02 22:43:36 -0300

    feat: add TotalAmountOfPoints challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/TotalAmountOfPoints.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.TotalPoints;
+
+public static class Kata
+{
+    public static int TotalPoints(string[] games)
+    {
+        int points = 0;
+
+        foreach (string game in games)
+        {
+            string[] score = game.Split(':');
+
+            int x = int.Parse(score[0]);
+            int y = int.Parse(score[1]);
+
+            if (x > y)
+                points += 3;
+            else if (x == y)
+                points += 1;
+        }
+        return points;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/TotalAmountOfPointsTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.TotalPoints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class TotalAmountOfPointsTests
+{
+    [Theory]
+    [InlineData(new string[] {"1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"}, 30)]
+    [InlineData(new string[] { "0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4" }, 0)]
+    [InlineData(new string[] { "1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4" }, 10)]
+
+    public void TotalPoints_GivenGameResults_ShouldReturnExpectedPoints(string[] games, int expected)
+    {
+        //Act
+        var result = Kata.TotalPoints(games);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
