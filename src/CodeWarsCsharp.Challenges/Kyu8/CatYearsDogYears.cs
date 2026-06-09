cffaa1858d6898f9fedb8c9f9d4fba80e22922b4
Wesley <wesleysbs@hotmail.com>
2026-06-09 20:10:03 -0300

    feat: add CatYearsDogYears challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/CatYearsDogYears.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class Dinglemouse
+{
+
+    public static int[] humanYearsCatYearsDogYears(int humanYears)
+    {
+        int catYears = 0;
+        int dogYears = 0;
+
+        if (humanYears >= 1)
+        {
+            catYears += 15;
+            dogYears += 15;
+        }
+
+        if (humanYears >= 2)
+        {
+            catYears += 9;
+            dogYears += 9;
+        }
+
+        if (humanYears > 2)
+        {
+            catYears += (humanYears - 2) * 4;
+            dogYears += (humanYears - 2) * 5;
+        }
+
+        return new int[] { humanYears, catYears, dogYears };
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/CatYearsDogYearsTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class CatYearsDogYearsTests
+{
+    [Fact]
+    public void HumanYearsToAnimalYears_GivenHumanYears_ShouldReturnCorrectCatAndDogYears()
+    {
+        //Arrange
+        var inputValue = 3;
+        var expectedResult = new int[] { 3, 28, 29 };
+
+        //Act
+        var result = Dinglemouse.humanYearsCatYearsDogYears(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
