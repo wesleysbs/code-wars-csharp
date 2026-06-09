ea6e7830e21ef18bc369f979582e0daad32ab0c6
Wesley <wesleysbs@hotmail.com>
2026-06-08 21:24:29 -0300

    feat: add MultiplicationTableForNumber challenge and tests (#49)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/MultiplicationTableForNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8.MultiplicationTableForNumber;
+
+public class Kata
+{
+    public static string MultiTable(int number)
+    {
+        string result = "";
+
+        for (int i = 1; i <= 10; i++)
+        {
+            result += $"{i} * {number} = {i * number}";
+
+            if (i < 10)
+            {
+                result += "\n";
+            }
+        }
+        return result;
+    }
+}
