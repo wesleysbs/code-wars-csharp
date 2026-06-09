02a1aa917ea5306abd9f898d924a55678a122cec
Wesley <wesleysbs@hotmail.com>
2026-06-08 21:58:45 -0300

    feat: add ReversedSequence challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ReversedSequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.ReversedSequence;
+
+public static class Kata
+{
+    public static int[] ReverseSeq(int n)
+    {
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = n - i;
+        }
+        return result;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ReversedSequenceTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.ReversedSequence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8.ReversedSequence;
+
+public class ReversedSequenceTests
+{
+    [Fact]
+    public void ReverseSeq_GivenPositiveInteger_ShouldReturnDescendingSequence()
+    {
+        //Arrange
+        var inputValue = 5;
+        var expectedResult = new int[] {5,4,3,2,1};
+
+        //Act
+        var result = Kata.ReverseSeq(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
