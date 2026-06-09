9a8c7b616a02e0f8d6d617f12c3b18b0838c50ff
Wesley <wesleysbs@hotmail.com>
2026-06-08 22:47:59 -0300

    feat: add IsHeGonnaSurvive challenge and tests (#51)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/IsHeGonnaSurviveTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.Hero;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8.Hero;
+
+public class IsHeGonnaSurviveTests
+{
+    [Theory]
+    [InlineData(10,5,true)]
+    [InlineData(100,40,true)]
+    [InlineData(4, 5, false)]
+    [InlineData(0, 1, false)]
+
+    public void Hero_GivenBulletsAndDragons_ShouldReturnExpectedResult(int bullets, int dragons, bool expected)
+    {
+        //Act
+        var result = Kata.Hero(bullets, dragons);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
