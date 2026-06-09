72dcd6b9dae095be10391ec5a0cec7524ec36caf
Wesley <wesleysbs@hotmail.com>
2026-06-08 22:46:26 -0300

    feat: add IsHeGonnaSurvive challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/IsHeGonnaSurvive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.Hero;
+
+public class Kata
+{
+    public static bool Hero(int bullets, int dragons)
+    {
+        int bulletsNecessary = dragons * 2;
+
+        if (bullets >= bulletsNecessary)
+        {
+            return true;
+        }
+       else
+        {
+            return false;
+        }
+    }
+}
