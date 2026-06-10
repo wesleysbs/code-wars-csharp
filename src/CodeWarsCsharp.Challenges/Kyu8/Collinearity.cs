937bb59e9b23ba49868c010bff052a893fbfe5a0
Wesley <wesleysbs@hotmail.com>
2026-06-10 13:07:48 -0300

    feat: add Collinearity challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/Collinearity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.Collinearity;
+
+public class Kata
+{
+    public static bool Collinearity(int x1, int y1, int x2, int y2)
+    {
+        return x1 * y2 == y1 * x2;
+    }
+}
+
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/CollinearityTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.Collinearity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class CollinearityTests
+{
+    [Theory]
+    [InlineData(1, 1, 1, 1, true)]
+    [InlineData(1, 1, 6, 1, false)]
+    [InlineData(1, -2, -2, 4, true)]
+    [InlineData(1, 2, 1, -2, false)]
+    [InlineData(0, 0, 0, 0, true)]
+    [InlineData(0, 1, 6, 0, false)]
+
+    public void Collinearity_GivenVectorCoordinates_ShouldReturnExpectedResult(int x1, int y1, int x2, int y2, bool expected)
+    {
+        //Act
+        var result = Kata.Collinearity(x1, y1, x2, y2);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
