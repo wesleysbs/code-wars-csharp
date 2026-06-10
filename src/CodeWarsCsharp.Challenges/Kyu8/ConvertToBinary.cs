441c917c70df44caa5806dbed4a8dca5b8c0908d
Wesley <wesleysbs@hotmail.com>
2026-06-09 22:02:01 -0300

    feat: add ConvertToBinary challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ConvertToBinary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ConvertToBinary
+{
+    public static int ToBinary(int n)
+    {
+        {
+            return int.Parse(Convert.ToString(n, 2));
+        }
+    }
+}
+
