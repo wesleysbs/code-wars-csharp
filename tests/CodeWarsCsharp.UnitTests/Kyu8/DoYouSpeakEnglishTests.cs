7e45406b2358b027283f8429b7da5b44a31d1219
Wesley <wesleysbs@hotmail.com>
2026-06-09 22:24:45 -0300

    feat: add DoYouSpeakEnglish challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/DoYouSpeakEnglishTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class DoYouSpeakEnglishTests
+{
+    [Theory]
+    [InlineData("hello world", false)]
+    [InlineData("englishENGLISHEnGlISHENglisH", true)]
+    [InlineData("egnlish", false)]
+    [InlineData("eNgliSh", true)]
+    [InlineData("", false)]
+    [InlineData("1234#$%%eNglish ;k9", true)]
+
+    public void SpeakEnglish_GivenInputString_ShouldDetectEnglishWord(string inputValue, bool expected)
+    {
+        //Act
+        var result = DoYouSpeakEnglish.SpeakEnglish(inputValue);
+
+        //Assert
+        Assert.Equal(result, expected);
+    }
+}
