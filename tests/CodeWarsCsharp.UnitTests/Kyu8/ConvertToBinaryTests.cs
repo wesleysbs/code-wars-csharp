4991bd40d4cfa226bc58e849c32991a7bc003c4a
Wesley <wesleysbs@hotmail.com>
2026-06-09 22:03:12 -0300

    feat: add ConvertToBinary challenge and tests (#54)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ConvertToBinaryTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ConvertToBinaryTests
+{
+    [Theory]
+    [InlineData(5,101)]
+    [InlineData(3,11)]
+
+    public void ConvertToBinary_GivenAnInteger_ShouldReturnCorrectBinaryValue(int inputValue, int expected)
+    {
+        //Act
+        var result = ConvertToBinary.ToBinary(inputValue);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
