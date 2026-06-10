a320511f61e5b9b52564fbd59afbaa57cb43e351
Wesley <wesleysbs@hotmail.com>
2026-06-09 22:44:28 -0300

    feat: add FiveWithoutNumbers challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/FiveWithoutNumbersTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class FiveWithoutNumbersTests
+{
+    [Fact]
+    public void UnusualFive_WhenCalled_ShouldReturnFive() 
+    {
+        //Act
+        var result = FiveWithoutNumbers.UnusualFive();
+
+        //Assert
+        Assert.Equal(5, result);
+    }
+}
