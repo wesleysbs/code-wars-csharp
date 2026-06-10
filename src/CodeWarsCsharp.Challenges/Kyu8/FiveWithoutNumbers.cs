2cb46f950a423a198c82f477e6e58e85461fb703
Wesley <wesleysbs@hotmail.com>
2026-06-09 22:45:00 -0300

    feat: add FiveWithoutNumbers challenge and tests (#56)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/FiveWithoutNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class FiveWithoutNumbers
+{
+    public static int UnusualFive()
+    {
+        return "hello".Length;
+    }
+}
+
