38f563c6fca79a6840a9a5d3f52ab441f32488b9
Wesley <wesleysbs@hotmail.com>
2026-06-09 22:26:10 -0300

    feat: add DoYouSpeakEnglish challenge and tests (#55)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/DoYouSpeakEnglish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class DoYouSpeakEnglish
+{
+    public static bool SpeakEnglish(string sentence)
+    {
+         return sentence.Contains("english", StringComparison.OrdinalIgnoreCase);
+    }
+}
+
