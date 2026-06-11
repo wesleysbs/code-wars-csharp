ac59efef58c37293cd60b3d777c0f77f962d79d8
Wesley <wesleysbs@hotmail.com>
2026-06-11 16:43:13 -0300

    feat: add DrawStairs challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/DrawStairs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.DrawStairs;
+
+public class Kata
+{
+    public static string DrawStairs(int n)
+    {
+        string result = "";
+
+        for (int i = 0; i < n; i++)
+        {
+            result += new string(' ', i) + "I";
+
+            if (i < n - 1)
+            {
+                result += "\n";
+            }
+        }
+
+        return result;
+    }
+}
