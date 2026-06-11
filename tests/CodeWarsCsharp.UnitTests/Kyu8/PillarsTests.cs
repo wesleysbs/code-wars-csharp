6689f03cadccc2924ef6a05a8dfa6ee7d56da986
Wesley <wesleysbs@hotmail.com>
2026-06-10 23:59:27 -0300

    feat: add Pillar challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/PillarsTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.Pillars;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class PillarsTests
+{
+    [Theory]
+    [InlineData(1, 10, 10, 0)]
+    [InlineData(2, 20, 25, 2000)]
+    [InlineData(11, 15, 30, 15270)]
+
+    public void Pillars_GivenNumberOfPillarsDistanceAndWidth_ShouldReturnDistanceInCentimeters(int numPill, int dist, int width, int expected)
+    {
+        //Act
+        var result = Kata.Pillars(numPill, dist, width);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
