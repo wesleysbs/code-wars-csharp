1e9550fda6081c9e97bffea162e115406245b20a
Wesley <wesleysbs@hotmail.com>
2026-06-10 23:34:12 -0300

    feat: add GravityFlip challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/GravityFlip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class GravityFlip
+{
+    public static int[] Flip(char dir, int[] arr)
+    {
+        Array.Sort(arr);
+        if (dir == 'L')
+            Array.Reverse(arr);
+        return arr;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/GravityFlipTest.cs
@@ -0,0 +1,41 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class GravityFlipTest
+{
+    [Fact]
+
+    public void Flip_GivenRightDirectionAndColumns_ShouldReturnExpectedOrder()
+    {
+        //Arrange
+        var inputValue = 'R';
+        var inputArray = new int[] { 3, 2, 1, 2 };
+        var expectedResult = new int[] { 1, 2, 2, 3 };
+
+        //Act
+        var result = GravityFlip.Flip(inputValue, inputArray);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+   }
+
+    [Fact]
+
+    public void Flip_GivenLeftDirectionAndColumns_ShouldReturnExpectedOrder()
+    {
+        //Arrange
+        var inputValue = 'L';
+        var inputArray = new int[] { 1, 4, 5, 3, 5 };
+        var expectedResult = new int[] { 5, 5, 4, 3, 1 };
+
+        //Act
+        var result = GravityFlip.Flip(inputValue, inputArray);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
