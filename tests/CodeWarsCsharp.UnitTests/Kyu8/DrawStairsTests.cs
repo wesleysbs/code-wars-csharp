978c38233f2c94378428f19a50b685e5bcc5fbc6
Wesley <wesleysbs@hotmail.com>
2026-06-11 16:44:10 -0300

    feat: add DrawStairs challenge and tests (#61)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/DrawStairsTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8.DrawStairs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class DrawStairsTests
+{
+    [Theory]
+    [InlineData(1, "I")]
+    [InlineData(2, "I\n I")]
+    [InlineData(3, "I\n I\n  I")]
+    [InlineData(4, "I\n I\n  I\n   I")]
+    public void DrawStairs_GivenNumberOfSteps_ShouldReturnCorrectStairPattern(int n, string expected)
+    {
+        //Act
+        var result = Kata.DrawStairs(n);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
