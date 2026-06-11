5498c1bfd81547068e92cdeb3ef40262ef493568
Wesley <wesleysbs@hotmail.com>
2026-06-11 00:25:17 -0300

    feat: add SumOfDifferencesInArray challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SumOfDifferencesInArrayTests.cs
@@ -0,0 +1,51 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SumOfDifferencesInArrayTests
+{
+    [Fact]
+    public void SumOfDifferences_GivenAnEmptyArray_ShouldReturnZero()
+    {
+        //Arrange
+        var inputValue = new int[] { };
+        var expectedResult = 0;
+
+        //Act
+        var result = SumOfDifferencesInArray.SumOfDifferences(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void SumOfDifferences_GivenAnArrayWithSingleElement_ShouldReturnZero()
+    {
+        //Arrange
+        var inputValue = new int[] {1};
+        var expectedResult = 0;
+
+        //Act
+        var result = SumOfDifferencesInArray.SumOfDifferences(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void SumOfDifferences_GivenAnArrayOfIntegers_ShouldReturnCorrectSum()
+    {
+        //Arrange
+        var inputValue = new int[] {0, 29, 24, 17};
+        var expectedResult = 29;
+
+        //Act
+        var result = SumOfDifferencesInArray.SumOfDifferences(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
