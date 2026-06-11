07c79b5192377379acdfca1d12f95803d3a6c8df
Wesley <wesleysbs@hotmail.com>
2026-06-11 00:00:15 -0300

    feat: add Pillars challenge and tests (#59)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/Pillars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8.Pillars;
+
+public class Kata
+{
+    public static int Pillars(int numPill, int dist, int width)
+    {
+        if (numPill == 1)
+            return 0;
+        int espacos = (numPill - 1) * dist * 100;
+        int middlePillarsWidth = (numPill - 2) * width;
+        return espacos + middlePillarsWidth;
+    }
+}
