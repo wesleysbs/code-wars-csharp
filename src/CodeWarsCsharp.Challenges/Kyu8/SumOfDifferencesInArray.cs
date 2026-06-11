68ff605c5ec19f5a9949bcb8ced6bf38b5bb76de
Wesley <wesleysbs@hotmail.com>
2026-06-11 00:25:53 -0300

    feat: add SumOfDifferencesInArray challenge and tests (#60)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SumOfDifferencesInArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SumOfDifferencesInArray
+{
+    public static int SumOfDifferences(int[] arr)
+    {
+        if (arr.Length <= 1)
+            return 0;
+
+        return arr.Max() - arr.Min();
+    }
+}
