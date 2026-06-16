fed8f19156272ec396f70a6596c422e0a0a76ee8
Wesley <wesleysbs@hotmail.com>
2026-06-15 22:37:32 -0300

    feat: add FindNearestSquareNumber challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/FindNearestSquareNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class FindNearestSquareNumber
+{
+    public static int NearestSq(int n)
+    {
+        var root = Math.Sqrt(n);
+        var nearestRoot = (int) Math.Round(root);
+        return nearestRoot * nearestRoot;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/FindNearestSquareNumberTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class FindNearestSquareNumberTests
+{
+    [Theory]
+    [InlineData(1,1)]
+    [InlineData(2,1)]
+    [InlineData(10, 9)]
+    [InlineData(111, 121)]
+    [InlineData(9999, 10000)]
+
+    public void NearestSq_GivenAnInteger_ShouldReturnExpectedResult(int n, int expected)
+    {
+        //Act
+        var result = FindNearestSquareNumber.NearestSq(n);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
