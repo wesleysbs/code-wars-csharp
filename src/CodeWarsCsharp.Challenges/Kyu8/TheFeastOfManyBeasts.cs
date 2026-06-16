44d7bfc820d229668e469accbd9216971319f831
Wesley <wesleysbs@hotmail.com>
2026-06-15 22:07:21 -0300

    feat: add TheFeastOfManyBeasts challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/TheFeastOfManyBeasts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class TheFeastOfManyBeasts
+{
+    public static bool Feast(string beast, string dish)
+    {
+        return beast[0] == dish[0] &&
+               beast[beast.Length - 1] == dish[dish.Length - 1];
+    }
+}
