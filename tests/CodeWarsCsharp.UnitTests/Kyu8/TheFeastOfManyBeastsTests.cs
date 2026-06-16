f00dfc228a56e16c7acb2862232a1b1402c0ae7d
Wesley <wesleysbs@hotmail.com>
2026-06-15 22:08:08 -0300

    feat: add TheFeastOfManyBeasts challenge and tests (#62)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/TheFeastOfManyBeastsTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class TheFeastOfManyBeastsTests
+{
+    [Theory]
+    [InlineData("great blue heron", "garlic naan", true)]
+    [InlineData("chickadee", "chocolate cake", true)]
+    [InlineData("brown bear", "bear claw", false)]
+    [InlineData("fox", "fish", false)]
+
+    public void Feast_GivenBeastAndDish_ShouldReturnExpectedResult(string beast, string dish, bool expected)
+    {
+        //Act
+        var result = TheFeastOfManyBeasts.Feast(beast, dish);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
