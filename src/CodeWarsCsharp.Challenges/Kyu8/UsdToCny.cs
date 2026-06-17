d4355baae13b5f504cc3951dade83f925aeb3489
Wesley <wesleysbs@hotmail.com>
2026-06-17 20:51:44 -0300

    feat: add UsdToCny challenge and tests (#66)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/UsdToCny.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class UsdToCny
+{
+    public static string Usdcny(int usd)
+    {
+        return $"{usd * 6.75:F2} Chinese Yuan";
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/UsdToCnyTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class UsdToCnyTests
+{
+    [Theory]
+    [InlineData(15, "101.25 Chinese Yuan")]
+    [InlineData(465, "3138.75 Chinese Yuan")]
+    [InlineData(4, "27.00 Chinese Yuan")]
+
+    public void Usdcny_GivenUsdAmount_ShouldReturnExpectedChineseYuan(int usd, string expected)
+    {
+        //Act
+        var result = UsdToCny.Usdcny(usd);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
