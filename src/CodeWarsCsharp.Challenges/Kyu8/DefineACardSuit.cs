c76ee07d10128528e00c8095bd71fa09d5cf1809
Wesley <wesleysbs@hotmail.com>
2026-06-16 23:18:52 -0300

    feat: add DefineACardSuit challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/DefineACardSuit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class DefineACardSuit
+{
+    public static string DefineSuit(string card)
+    {
+        char suit = card[card.Length - 1];
+
+        switch (suit)
+        {
+            case '♣':
+                return "clubs";
+
+            case '♥':
+                return "hearts";
+
+            case '♠':
+                return "spades";
+
+            case '♦':
+                return "diamonds";
+
+            default:
+                throw new ArgumentException("Invalid card suit");
+        }
+    }
+}
