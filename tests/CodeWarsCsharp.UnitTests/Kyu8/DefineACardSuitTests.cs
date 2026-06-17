c74d9b7d932dc7195ce33b52942de5edefa9752d
Wesley <wesleysbs@hotmail.com>
2026-06-16 23:19:41 -0300

    feat: add DefineACardSuit challenge and tests (#64)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/DefineACardSuitTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class DefineACardSuitTests
+{
+    [Theory]
+    [InlineData("3♣", "clubs")]
+    [InlineData("Q♠", "spades")]
+    [InlineData("9♦", "diamonds")]
+    [InlineData("J♥", "hearts")]
+
+    public void DefineSuit_GivenCard_ShouldReturnExpectedSuit(string card, string expected)
+    {
+        //Act
+        var result = DefineACardSuit.DefineSuit(card);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
