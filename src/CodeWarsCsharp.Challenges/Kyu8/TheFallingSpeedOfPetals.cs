d5ff172d9ce6d14096cb70265bdf0b281dc77961
Wesley <wesleysbs@hotmail.com>
2026-06-17 20:28:29 -0300

    feat: add TheFallingSpeedOfPetals challenge and tests (#65)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/TheFallingSpeedOfPetals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class TheFallingSpeedOfPetals
+{
+    public static double SakuraFall(double v)
+    {
+        if (v <= 0)
+        {
+            return 0;
+        }
+
+        return 400.0 / v;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/TheFallingSpeedOfPetalsTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class TheFallingSpeedOfPetalsTests
+{
+    [Theory]
+    [InlineData(0.0,0.0)]
+    [InlineData(-1.0,0.0)]
+    [InlineData(5.0, 80.0)]
+    [InlineData(10.0, 40.0)]
+    [InlineData(20.0, 20.0)]
+
+    public void SakuraFall_GivenVelocity_ShouldReturnExpectedTime(double v, double expected)
+    {
+        //Act
+        var result = TheFallingSpeedOfPetals.SakuraFall(v);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
