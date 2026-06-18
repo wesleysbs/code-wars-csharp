77e873cd6a4b0de5331fa92f5bde44caa0352ca0
Wesley <wesleysbs@hotmail.com>
2026-06-17 22:02:53 -0300

    feat: add SumTheStrings challenge and tests (#67)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SumTheStrings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SumTheStrings
+{
+    public static string StringsSum(string s1, string s2)
+    {
+        int a = string.IsNullOrEmpty(s1) ? 0 : int.Parse(s1);
+        int b = string.IsNullOrEmpty(s2) ? 0 : int.Parse(s2);
+
+        return (a + b).ToString();
+    }
+}
