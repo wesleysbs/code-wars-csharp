523af6bcb7040d0b85e748eee7d1a2d7e129f0c7
Wesley <wesleysbs@hotmail.com>
2026-06-23 20:57:48 -0300

    feat: add CapitalizationAndMutability challenge and tests (#69)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/CapitalizationAndMutability.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class CapitalizationAndMutability
+{
+    public static string CapitalizeWord(string word)
+    {
+        return char.ToUpper(word[0]) + word.Substring(1);
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/CapitalizationAndMutabilityTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class CapitalizationAndMutabilityTests
+{
+    [Theory]
+    [InlineData("word","Word")]
+    [InlineData("i","I")]
+    [InlineData("Hello", "Hello")]
+
+    public void CapitalizeWord_GivenAWord_ShouldCapitalizeFirstLetter(string word, string expected)
+    {
+        //Act
+        var result = CapitalizationAndMutability.CapitalizeWord(word);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
