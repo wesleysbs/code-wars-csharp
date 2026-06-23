6706a56d7c63b6dca553bf5f9ed02437004e4ae0
Wesley <wesleysbs@hotmail.com>
2026-06-22 22:16:54 -0300

    feat: add ContaminationString challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ContaminationStringTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ContaminationStringTests
+{
+    [Theory]
+    [InlineData("abc", "z", "zzz")]
+    [InlineData("", "z", "")]
+    [InlineData("abc", "", "")]
+    [InlineData("a", "z", "z")]
+    [InlineData("abc", "!", "!!!")]
+    [InlineData("aaaaa", "x", "xxxxx")]
+
+    public void Contamination_GivenValidInputs_ShouldReturnContaminatedText(string text, string character, string expected)
+    {
+        //Act
+        var result = ContaminationString.Contamination(text, character);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
