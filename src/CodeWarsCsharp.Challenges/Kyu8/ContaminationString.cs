fb95df74c3d8e8cec94dc6682ad1bd0a16747ba3
Wesley <wesleysbs@hotmail.com>
2026-06-22 22:18:03 -0300

    feat: add ContaminationString challenge and tests (#68)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ContaminationString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ContaminationString
+{
+    public static string Contamination(string text, string character)
+    {
+        if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(character))
+            return "";
+
+        return new string(character[0], text.Length);
+    }
+}
