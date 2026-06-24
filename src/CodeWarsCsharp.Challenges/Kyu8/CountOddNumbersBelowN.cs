30cb8853fb52b5e4ab87a4405732c4a9a6d6c444
Wesley <wesleysbs@hotmail.com>
2026-06-23 21:15:50 -0300

    feat: add CountOddNumbersBellowN challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/CountOddNumbersBelowN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class CountOddNumbersBelowN
+{
+    public static ulong OddCount(ulong n)
+    {
+        return n / 2;
+    }
+}
