c8886b719e0fb49814c3a48121a46ee9fd786ce9
Wesley <wesleysbs@hotmail.com>
2026-07-01 17:55:25 -0300

    feat: add InvertValues challenge and tests (#71)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/InvertValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class Invert
+{
+    public static int[] InvertValues(int[] input)
+    {
+        int[] result = new int[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            result[i] = -input[i];
+        }
+        return result;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/InvertValuesTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class InvertValuesTests
+{
+    [Fact]
+
+    public void InvertValues_GivenIntegerArray_ShouldReturnInvertedArray()
+    {
+        //Arrange
+        var inputValue = new int[] {1, -2, 3, -4, 5};
+        var expectedValue = new int[] {-1, 2, -3, 4, -5};
+
+        //Act
+        var result = Invert.InvertValues(inputValue);
+
+        //Assert
+        Assert.Equal(expectedValue, result);
+    }
+}
