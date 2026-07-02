9fcb86709e1ad0cc749a94dfa8efdf68e3e16a2e
Wesley <wesleysbs@hotmail.com>
2026-07-02 18:39:48 -0300

    feat: add SeatsInTheater challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SeatsInTheater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SeatsInTheater
+{
+    public static int Seats(int nCols, int nRows, int col, int row)
+    {
+        return (nRows - row) * (nCols - col + 1);
+    }
+}
