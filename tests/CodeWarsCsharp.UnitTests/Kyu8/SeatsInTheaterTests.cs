096bce1bb5f436f5a1f34879682de7b0dfd5f551
Wesley <wesleysbs@hotmail.com>
2026-07-02 18:40:45 -0300

    feat: add SeatsInTheater challenge and tests (#72)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SeatsInTheaterTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SeatsInTheaterTests
+{
+    [Theory]
+    [InlineData(16, 11, 5, 3, 96)]
+    [InlineData(1, 1, 1, 1, 0)]
+    [InlineData(1000, 1000, 1000, 1000, 0)]
+    [InlineData(13, 6, 8, 3, 18)]
+
+    public void TheaterSeats_GivenPosition_ShouldReturnNumberOfPeopleAffected(int nCols, int nRows, int col, int row, int expected)
+    {
+        //Act
+        var result = SeatsInTheater.Seats(nCols, nRows, col, row);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
