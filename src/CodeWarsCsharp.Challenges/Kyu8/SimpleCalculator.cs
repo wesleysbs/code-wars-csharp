0e99f618dcfba484c5547836859633e6870fc752
Wesley <wesleysbs@hotmail.com>
2026-07-02 19:36:13 -0300

    feat: add SimpleCalculator challenge and tests (#74)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SimpleCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SimpleCalculator
+{
+    public static double Calculator(double a, double b, char op)
+    {
+        switch (op)
+        {
+            case '+':
+                return a + b;
+
+            case '-':
+                return a - b;
+
+            case '*':
+                return a * b;
+
+            case '/':
+                return a / b;
+
+            default:
+                throw new ArgumentException("Invalid operator.");
+        }
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SimpleCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SimpleCalculatorTests
+{
+    [Theory]
+    [InlineData(6, 2, '+', 8)]
+    [InlineData(4, 3, '-', 1)]
+    [InlineData(5, 5, '*', 25)]
+    [InlineData(5, 4, '/', 1.25)]
+
+    public void Calculator_GivenTwoNumbersAndOperator_ShouldReturnCorrectResult(double a, double b, char op, double expected)
+    {
+        //Act
+        var result = SimpleCalculator.Calculator(a, b, op);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Calculator_GivenInvalidOperator_ShouldThrowArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            SimpleCalculator.Calculator(1, 2, '&'));
+    }
+}
