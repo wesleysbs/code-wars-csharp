e6cafc672be9b1704f883c49cfa28cd40bb18438
Wesley <wesleysbs@hotmail.com>
2026-07-02 19:07:54 -0300

    feat: add WillThereBeEnoughSpace challenge and tests (#73)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/WillThereBeEnoughSpace.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class WillThereBeEnoughSpace
+{
+    public static int Enough(int cap, int on, int wait)
+    {
+        return on + wait <= cap ? 0 : on + wait - cap;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/WillThereBeEnoughSpaceTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class WillThereBeEnoughSpaceTests
+{
+    [Theory]
+    [InlineData(10, 5, 5, 0)]
+    [InlineData(100, 60, 50, 10)]
+    [InlineData(20, 5, 5, 0)]
+
+    public void Enough_GivenBusCapacityAndPassengers_ShouldReturnPassengersUnableToBoard(int cap, int on, int wait, int expected)
+    {
+        //Act
+        var result = WillThereBeEnoughSpace.Enough(cap, on, wait);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
