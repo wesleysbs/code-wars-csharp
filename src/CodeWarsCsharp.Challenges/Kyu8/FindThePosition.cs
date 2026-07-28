73b9a2d58343b932dcc37f096fed01c3cb2cd908
Wesley <wesleysbs@hotmail.com>
2026-07-27 21:59:19 -0300

    feat: add FindThePosition challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/FindThePosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class FindThePosition
+{
+    public static string Position(char alphabet)
+    {
+        int position = alphabet - 'a' + 1;
+        return $"Position of alphabet: {position}";
+    }
+}
