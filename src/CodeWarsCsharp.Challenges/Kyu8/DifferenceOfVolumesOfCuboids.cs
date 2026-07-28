7dc270ff921d16cecbc4d0512691a0315cd78074
Wesley <wesleysbs@hotmail.com>
2026-07-27 21:22:00 -0300

    feat: add DifferenceOfVolumesOfCuboids challenge and tests (#75)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/DifferenceOfVolumesOfCuboids.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class DifferenceOfVolumesOfCuboids
+{
+    public static int FindDifference(int[] a, int[] b)
+    {
+        int volumeA = a[0] * a[1] * a[2];
+        int volumeB = b[0] * b[1] * b[2];
+
+        return Math.Abs(volumeA - volumeB);
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/DifferenceOfVolumesOfCuboidsTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class DifferenceOfVolumesOfCuboidsTests
+{
+    [Fact]
+
+    public void FindDifference_GivenTwoCuboids_ShouldReturnVolumeDifference()
+    {
+        //Arrange
+        var firstInput = new int[] { 3, 2, 5 };
+        var secondInput = new int[] { 1, 4, 4 };
+
+        var expectedResult = 14;
+
+        //Act
+        var result = DifferenceOfVolumesOfCuboids.FindDifference(firstInput, secondInput);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
