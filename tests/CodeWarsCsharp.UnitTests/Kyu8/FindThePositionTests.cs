95b8631226104aba5b96142f97a176ff59944479
Wesley <wesleysbs@hotmail.com>
2026-07-27 22:00:02 -0300

    feat: add FindThePosition challenge and tests (#76)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/FindThePositionTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class FindThePositionTests
+{
+    [Theory]
+    [InlineData('a', "Position of alphabet: 1")]
+    [InlineData('z', "Position of alphabet: 26")]
+    [InlineData('k', "Position of alphabet: 11")]
+
+    public void Position_GivenACharacter_ShouldReturnItsPositionInAlphabet(char alphabet, string expected)
+    {
+        //Act
+        var result = FindThePosition.Position(alphabet);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
