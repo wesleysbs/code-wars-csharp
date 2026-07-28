c7444b4d607d83099a052d5be2149ccefbd1e34b
Wesley <wesleysbs@hotmail.com>
2026-07-27 22:26:38 -0300

    feat: add SwitchItUp challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SwitchItUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SwitchItUp
+{
+    public static string NumberToWord(int number)
+    {
+        switch(number)
+        {
+            case 0:
+                return "Zero";
+
+            case 1:
+                return "One";
+
+            case 2:
+                return "Two";
+
+            case 3:
+                return "Three";
+
+            case 4:
+                return "Four";
+
+            case 5:
+                return "Five";
+
+            case 6:
+                return "Six";
+
+            case 7:
+                return "Seven";
+
+            case 8:
+                return "Eight";
+
+            case 9:
+                return "Nine";
+
+            default:
+                throw new ArgumentException("Number must be between 0 and 9.");
+        }
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SwitchItUpTests.cs
@@ -0,0 +1,30 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SwitchItUpTests
+{
+    [Theory]
+    [InlineData(0, "Zero")]
+    [InlineData(9, "Nine")]
+    [InlineData(5, "Five")]
+
+    public void NumberToWord__GivenAValidNumber_ShouldReturnCorrespondingWord(int number, string expected)
+    {
+        //Act
+        var result = SwitchItUp.NumberToWord(number);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void NumberToWord_GivenInvalidNumber_ShouldThrowArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            SwitchItUp.NumberToWord(15));
+    }
+}
