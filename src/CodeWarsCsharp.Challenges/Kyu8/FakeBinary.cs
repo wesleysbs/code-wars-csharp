30108728466d76e403bc8e86a3458a6a3462cb0b
Wesley <wesleysbs@hotmail.com>
2026-07-28 22:17:27 -0300

    feat: add FakeBinary challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/FakeBinary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class FakeBinary
+{
+    public static string FakeBin(string digits)
+    {
+        string newString = "";
+
+        for (int i = 0; i < digits.Length; i++)
+        {
+            if (digits[i] >= '5')
+            {
+                newString += '1';
+            }
+
+            else
+            {
+                newString += '0';
+            }
+        }
+        return newString;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/FakeBinaryTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class FakeBinaryTests
+{
+    [Theory]
+    [InlineData("45385593107843568", "01011110001100111")]
+    [InlineData("509321967506747", "101000111101101")]
+    [InlineData("366058562030849490134388085", "011011110000101010000011011")]
+
+    public void FakeBin_GivenStringOfDigits_ShouldReturnCorrectBinary(string digits, string expected)
+    {
+        //Act
+        var result = FakeBinary.FakeBin(digits);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
