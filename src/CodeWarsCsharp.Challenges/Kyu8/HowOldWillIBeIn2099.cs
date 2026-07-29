8d5a5b070abc4c14369b8e9a128d4c10101efbed
Wesley <wesleysbs@hotmail.com>
2026-07-29 10:10:58 -0300

    feat: add HowOldWillIBeIn2099 challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/HowOldWillIBeIn2099.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class HowOldWillIBeIn2099
+{
+    public static string CalculateAge(int birth, int yearTo)
+    {
+        int age = Math.Abs(yearTo - birth);
+        string yearWord = age == 1 ? "year" : "years";
+
+        if (birth > yearTo)
+        {
+            return $"You will be born in {age} {yearWord}.";
+        }
+
+        else if (birth == yearTo)
+        {
+            return $"You were born this very year!";
+        }
+
+        else
+        {
+            return $"You are {age} {yearWord} old.";
+        }  
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/HowOldWillIBeIn2099Tests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class HowOldWillIBeIn2099Tests
+{
+    [Theory]
+    [InlineData(2003, 2020, "You are 17 years old.")]
+    [InlineData(2019, 2020, "You are 1 year old.")]
+    [InlineData(2003, 2003, "You were born this very year!")]
+    [InlineData(2020, 2003, "You will be born in 17 years.")]
+    [InlineData(2020, 2019, "You will be born in 1 year.")]
+
+    public void CalculateAge_GivenBirthYearAndTargetYear_ShouldReturnCorrectMessage(int birth, int yearTo, string expected)
+    {
+        //Act
+        var result = HowOldWillIBeIn2099.CalculateAge(birth, yearTo);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
