6366532d287b7d352e8bafb5fd631ae757024455
Wesley <wesleysbs@hotmail.com>
2026-07-29 10:28:15 -0300

    feat: add ConvertANumberToAString challenge and tests (#81)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ConvertANumberToAString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ConvertANumberToAString
+{
+    public static string NumberToString(int num)
+    {
+        return num.ToString();
+    }
+}
