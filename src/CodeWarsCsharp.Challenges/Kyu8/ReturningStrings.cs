5f4c2405d9378c82f563be2a8c1a7453d7b5bee0
Wesley <wesleysbs@hotmail.com>
2026-07-29 10:44:51 -0300

    feat: add ReturningStrings challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ReturningStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ReturningStrings
+{
+    public static string Greet(string name)
+    {
+        return $"Hello, {name} how are you doing today?";
+    }
+}
