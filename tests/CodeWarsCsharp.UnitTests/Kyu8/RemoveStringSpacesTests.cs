e042f32cbed00db6f9f73cfff9a347806e655287
Wesley <wesleysbs@hotmail.com>
2026-07-28 22:37:08 -0300

    feat: add RemoveStringSpaces challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/RemoveStringSpacesTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class RemoveStringSpacesTests
+{
+    [Theory]
+    [InlineData("8 j 8   mBliB8g  imjB8B8  jl  B", "8j8mBliB8gimjB8B8jlB")]
+    [InlineData("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd", "88Bifk8hB8BB8BBBB888chl8BhBfd")]
+    [InlineData("8aaaaa dddd r     ", "8aaaaaddddr")]
+
+    public void NoSpace_GivenStringWithSpaces_ShouldReturnStringWithoutSpaces(string input, string expected)
+    {
+        //Act
+        var result = RemoveStringSpaces.NoSpace(input);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
