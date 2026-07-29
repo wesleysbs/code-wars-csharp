50c711f6a4ecbadb1341d92f6f1a37f085e41d73
Wesley <wesleysbs@hotmail.com>
2026-07-29 10:27:32 -0300

    feat: add ConvertANumberToAString challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ConvertANumberToAStringTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ConvertANumberToAStringTests
+{
+    [Theory]
+    [InlineData(123, "123")]
+    [InlineData(0, "0")]
+    [InlineData(-5, "-5")]
+
+    public void NumberToString_GivenInteger_ShouldConvertToAString(int num, string expected)
+    {
+        //Act
+        var result = ConvertANumberToAString.NumberToString(num);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
