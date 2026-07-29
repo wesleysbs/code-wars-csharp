57800c3b467dd1a47d051a03259e44adccf18748
Wesley <wesleysbs@hotmail.com>
2026-07-29 10:45:47 -0300

    feat: add ReturningStrings challenge and tests (#82)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ReturningStringsTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ReturningStringsTests
+{
+    [Theory]
+    [InlineData("Ryan", "Hello, Ryan how are you doing today?")]
+    [InlineData("Emma", "Hello, Emma how are you doing today?")]
+    [InlineData("William", "Hello, William how are you doing today?")]
+
+    public void Greet_GivenAName_ShouldReturnCorrectMessage(string name, string expected)
+    {
+        //Act
+        var result = ReturningStrings.Greet(name);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
