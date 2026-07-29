074d1739d0b8f6f61ddc7df4cbf1a51d6df24f5e
Wesley <wesleysbs@hotmail.com>
2026-07-28 22:38:10 -0300

    feat: add RemoveStringSpaces challenge and tests (#79)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/RemoveStringSpaces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class RemoveStringSpaces
+{
+    public static string NoSpace(string input)
+    {
+        return input.Replace(" ", string.Empty);
+    }
+}
