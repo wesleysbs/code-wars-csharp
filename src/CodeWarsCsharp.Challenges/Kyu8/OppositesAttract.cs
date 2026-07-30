02dd20e28aecb67bce67f9c400a5acfd6e93fdae
Wesley <wesleysbs@hotmail.com>
2026-07-29 23:06:59 -0300

    feat: add OppositesAttract challenge and tests (#83)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/OppositesAttract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class OppositesAttract
+{
+    public static bool LoveFunc(int flower1, int flower2)
+    {
+        return flower1 % 2 != flower2 % 2;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/OppositesAttractTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class OppositesAttractTests
+{
+    [Theory]
+    [InlineData(1, 4, true)]
+    [InlineData(2, 2, false)]
+    [InlineData(3, 5, false)]
+    [InlineData(6, 7, true)]
+
+    public void LoveFunc_GivenTwoIntegers_ShouldReturnExpectedResult(int flower1, int flower2, bool expected)
+    {
+        //Act
+        var result = OppositesAttract.LoveFunc(flower1, flower2);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
