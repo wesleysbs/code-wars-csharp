8f0236b11b6c1e88a25a5508173e7955c369615e
Wesley <wesleysbs@hotmail.com>
2026-07-30 22:40:02 -0300

    feat: add DrinkAbout challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/DrinkAbout.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class DrinkAbout
+{
+    public static string PeopleWithAgeDrink(int old)
+    {
+        if (old < 14)
+        {
+            return "drink toddy";
+        }
+
+        else if (old < 18)
+        {
+            return "drink coke";
+        }
+
+        else if (old < 21)
+        {
+            return "drink beer";
+        }
+
+        else
+        {
+            return "drink whisky";
+        }
+    }
+}
