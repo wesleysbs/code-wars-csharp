811a0b38acdc567446796fa660a4710daa3f4faf
Wesley <wesleysbs@hotmail.com>
2026-07-30 22:41:49 -0300

    feat: add DrinkAbout challenge and tests (#84)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/DrinkAboutTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class DrinkAboutTests
+{
+    [Theory]
+    [InlineData(13, "drink toddy")]
+    [InlineData(14, "drink coke")]
+    [InlineData(18, "drink beer")]
+    [InlineData(21, "drink whisky")]
+
+
+    public void PeopleWithAgeDrink_GivenPeopleAge_ShouldReturnSuitableDrink(int old, string expected)
+    {
+        //Act
+        var result = DrinkAbout.PeopleWithAgeDrink(old);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
