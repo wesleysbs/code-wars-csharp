a8656203163d5b6407aab8b6c019726b3057f1eb
Wesley <wesleysbs@hotmail.com>
2026-07-30 23:09:42 -0300

    feat: add BasicMathematicalOperations challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/BasicMathematicalOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class BasicMathematicalOperations
+{
+    public static double BasicOp(char operation, double value1, double value2)
+    {
+        switch (operation)
+        {
+
+            case '+':
+                return value1 + value2;
+
+            case '-':
+                return value1 - value2;
+
+            case '*':
+                return value1 * value2;
+
+            case '/':
+                return value1 / value2;
+
+            default:
+                throw new ArgumentException("Choose a valid operator");
+        }
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/BasicMathematicalOperationsTests.cs
@@ -0,0 +1,32 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class BasicMathematicalOperationsTests
+{
+    [Theory]
+    [InlineData('+', 4, 7, 11)]
+    [InlineData('-', 15, 18, -3)]
+    [InlineData('*', 5, 5, 25)]
+    [InlineData('/', 49, 7, 7)]
+
+    public void BasicOp_GivenAnOperation_ShouldReturnCorrectCalculation(char operation, double value1, double value2, double expected)
+    {
+        //Act
+        var result = BasicMathematicalOperations.BasicOp(operation, value1, value2);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+
+    public void BasicOp_GivenInvalidOperation_ShouldThrowArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            BasicMathematicalOperations.BasicOp('%', 5, 3));
+    }
+}
