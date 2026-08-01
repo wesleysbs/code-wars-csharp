c4138f0371e8a7e1a5a90c736bac607eb39d28c4
Wesley <wesleysbs@hotmail.com>
2026-07-31 21:58:34 -0300

    feat: add GetPlanetNameById challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/GetPlanetNameByIdTests.cs
@@ -0,0 +1,36 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class GetPlanetNameByIdTests
+{
+    [Theory]
+    [InlineData(1, "Mercury")]
+    [InlineData(2, "Venus")]
+    [InlineData(3, "Earth")]
+    [InlineData(4, "Mars")]
+    [InlineData(5, "Jupiter")]
+    [InlineData(6, "Saturn")]
+    [InlineData(7, "Uranus")]
+    [InlineData(8, "Neptune")]
+
+    public void GetPlanetName_GivenAnId_ShouldReturnCorrectPlanet(int id, string expected)
+    {
+        //Act
+        var result = GetPlanetNameById.GetPlanetName(id);
+
+        //Assert
+        Assert.Equal(expected, result );
+    }
+
+    [Fact]
+    
+    public void GetPlanetName_GivenInvalidId_ShouldThrowArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        GetPlanetNameById.GetPlanetName(9));
+    }
+}
