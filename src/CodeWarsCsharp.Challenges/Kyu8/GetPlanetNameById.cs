c98aacffbccd7cd4bf057b8d84c8f57f06c09f3e
Wesley <wesleysbs@hotmail.com>
2026-07-31 22:00:25 -0300

    feat: add GetPlanetNameById challenge and tests (#87)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/GetPlanetNameById.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class GetPlanetNameById
+{
+    public static string GetPlanetName(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                return "Mercury";
+
+            case 2:
+                return "Venus";
+
+            case 3:
+                return "Earth";
+
+            case 4:
+                return "Mars";
+
+            case 5:
+                return "Jupiter";
+
+            case 6:
+                return "Saturn";
+
+            case 7:
+                return "Uranus";
+
+            case 8:
+                return "Neptune";
+
+            default:
+                throw new ArgumentException("Choose a valid number");
+        }
+    }
+}
