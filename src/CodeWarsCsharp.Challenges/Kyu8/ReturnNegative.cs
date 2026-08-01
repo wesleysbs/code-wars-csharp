3019510268c39469ad630820adfec68cbf8558b6
Wesley <wesleysbs@hotmail.com>
2026-07-31 21:20:35 -0300

    feat: add ReturnNegative challenge and tests (#86)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ReturnNegative.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ReturnNegative
+{
+    public static int MakeNegative(int number)
+    {
+        return -Math.Abs(number);
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ReturnNegativeTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ReturnNegativeTests
+{
+    [Theory]
+    [InlineData(1, -1)]
+    [InlineData(-5, -5)]
+    [InlineData(0, 0)]
+
+    public void MakeNegative_GivenAnInteger_ShouldReturnNegativeValue(int number, int expected)
+    {
+        //Act
+        var result = ReturnNegative.MakeNegative(number);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
