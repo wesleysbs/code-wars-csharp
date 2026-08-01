b3b332966f850df52b01ffd52dcbbbe93f551110
Wesley <wesleysbs@hotmail.com>
2026-07-31 21:36:45 -0300

    feat: add KeepUpTheHoop challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/KeepUpTheHoop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class KeepUpTheHoop
+{
+    public static string HoopCount(int n)
+    {
+        return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/KeepUpTheHoopTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class KeepUpTheHoopTests
+{
+    [Theory]
+    [InlineData(10, "Great, now move on to tricks")]
+    [InlineData(9, "Keep at it until you get it")]
+    [InlineData(22, "Great, now move on to tricks")]
+    [InlineData(0, "Keep at it until you get it")]
+
+    public void HoopCount_GivenNumberOfHoops_ShouldReturnCorrectMessage(int n, string expected)
+    {
+        //Act
+        var result = KeepUpTheHoop.HoopCount(n);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
