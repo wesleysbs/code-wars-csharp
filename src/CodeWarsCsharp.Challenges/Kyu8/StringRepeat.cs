4a31f5cde967e80f22350f110623a5f8f4025408
Wesley <wesleysbs@hotmail.com>
2026-08-01 21:45:44 -0300

    feat: add StringRepeat challenge and tests (#90)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/StringRepeat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class StringRepeat
+{
+    public static string RepeatStr(int n, string s)
+    {
+        string result = "";
+        for (int i = 0; i < n; i++)
+        {
+            result += s;
+        }
+        return result;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/StringRepeatTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class StringRepeatTests
+{
+    [Theory]
+    [InlineData(3, "*", "***")]
+    [InlineData(4, "", "")]
+    [InlineData(5, "Hello", "HelloHelloHelloHelloHello")]
+    [InlineData(0, "mystery", "")]
+
+    public void RepeatStr_GivenCountAndString_ShouldReturnRepeatedString(int n, string s, string expected)
+    {
+        //Act
+        var result = StringRepeat.RepeatStr(n, s);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
