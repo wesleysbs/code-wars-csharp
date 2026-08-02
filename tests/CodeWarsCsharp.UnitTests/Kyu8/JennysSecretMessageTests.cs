00130bc4c98f089c023edf15f05af9558e632657
Wesley <wesleysbs@hotmail.com>
2026-08-01 21:06:38 -0300

    feat: add JennysSecretMessage challenge and tests (#88)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/JennysSecretMessageTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class JennysSecretMessageTests
+{
+    [Theory]
+    [InlineData("Jim", "Hello, Jim!")]
+    [InlineData("Johnny", "Hello, my love!")]
+    [InlineData("Jane", "Hello, Jane!")]
+
+    public void Greet_GivenAName_ShouldReturnCorrectMessage(string name, string expected)
+    {
+        //Act
+        var result = JennysSecretMessage.Greet(name);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
