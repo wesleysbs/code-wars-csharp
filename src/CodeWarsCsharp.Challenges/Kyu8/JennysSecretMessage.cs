b0484a48fa29dc297d7212904ba571a4bb602476
Wesley <wesleysbs@hotmail.com>
2026-08-01 21:05:46 -0300

    feat: add JennysSecretMessage challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/JennysSecretMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class JennysSecretMessage
+{
+    public static string Greet(string name)
+    {
+        if (name == "Johnny")
+        {
+            return "Hello, my love!";
+        }
+        else
+        {
+            return $"Hello, {name}!";
+        }
+    }
+}
