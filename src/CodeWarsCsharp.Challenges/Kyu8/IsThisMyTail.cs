05ea676a48be8b49617ec09d14e93c02e5eb3404
Wesley <wesleysbs@hotmail.com>
2026-08-01 21:28:51 -0300

    feat: add IsThisMyTail challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/IsThisMyTail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class IsThisMyTail
+{
+    public static bool CorrectTail(string body, string tail)
+    {
+        string sub = body.Substring(body.Length - (tail.Length));
+        return sub == tail;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/IsThisMyTailTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class IsThisMyTailTests
+{
+    [Theory]
+    [InlineData("Fox", "x", true)]
+    [InlineData("Emu", "t", false)]
+    [InlineData("Rhino", "o", true)]
+    [InlineData("Giraffe", "d", false)]
+
+
+    public void CorrectTail_GivenBodyAndTail_ShouldReturnExpectedResult(string body, string tail, bool expected)
+    {
+        //Act
+        var result = IsThisMyTail.CorrectTail(body, tail);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
