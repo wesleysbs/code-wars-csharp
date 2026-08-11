fc22b84131cabd1acca9caef70974feaaebc6410
Wesley <wesleysbs@hotmail.com>
2026-08-10 23:43:21 -0300

    feat: add ConvertAStringToANumber challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/ConvertAStringToANumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class ConvertAStringToANumber
+{
+    public static int StringToNumber(String str)
+    {
+        return int.Parse(str);
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/ConvertAStringToANumberTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class ConvertAStringToANumberTests
+{
+    [Theory]
+    [InlineData("1234", 1234)]
+    [InlineData("0", 0)]
+    [InlineData("-7", -7)]
+    public void StringToNumber_GivenNumericString_ShouldReturnCorrectNumber(string str, int expected)
+    {
+        //Act
+        var result = ConvertAStringToANumber.StringToNumber(str);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
