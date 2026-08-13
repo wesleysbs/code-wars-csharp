83d37f830568b4810a684e9cfa969b95ea5bb8af
Wesley <wesleysbs@hotmail.com>
2026-08-12 21:18:22 -0300

    feat: add IsNDivisibleByXAndY challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/IsNDivisibleByXAndY.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class IsNDivisibleByXAndY
+{
+    public static bool IsDivisible(int n, int x, int y)
+    {
+        return n % x == 0 && n % y == 0;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/IsNDivisibleByXAndYTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class IsNDivisibleByXAndYTests
+{
+    [Theory]
+    [InlineData(12, 4, 3, true)]
+    [InlineData(3, 3, 4, false)]
+    [InlineData(8, 3, 4, false)]
+    [InlineData(12, 7, 5, false)]
+
+    public void IsDivisible_GivenThreeIntegers_ShouldReturnCorrectResult(int n, int x, int y, bool expected)
+    {
+        //Act
+        var result = IsNDivisibleByXAndY.IsDivisible(n, x, y);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
