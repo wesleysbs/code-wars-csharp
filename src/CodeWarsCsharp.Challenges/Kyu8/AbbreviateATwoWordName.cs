982f87bc8afa563762a0a4a65a5f663960f0d800
Wesley <wesleysbs@hotmail.com>
2026-08-12 21:36:55 -0300

    feat: add AbbreviateATwoWordName challenge and tests (#93)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/AbbreviateATwoWordName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class AbbreviateATwoWordName
+{
+    public static string AbbrevName(string name)
+    {
+        string[] words = name.Split(' ');
+
+        return $"{words[0][0].ToString().ToUpper()}.{words[1][0].ToString().ToUpper()}";
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/AbbreviateATwoWordNameTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class AbbreviateATwoWordNameTests
+{
+    [Theory]
+    [InlineData("Sam Harris", "S.H")]
+    [InlineData("patrick feenan", "P.F")]
+    [InlineData("P Favuzzi", "P.F")]
+
+    public void AbbrevName_GivenAName_ShouldReturnTheInitials(string name, string expected)
+    {
+        //Act
+        var result = AbbreviateATwoWordName.AbbrevName(name);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
