87efd0d8bd3981e381b5bf71a5217cf670d42953
Wesley <wesleysbs@hotmail.com>
2026-08-25 10:31:56 -0300

    feat: add Multiply challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/Multiply.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class CustomMath
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/MultiplyTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class MultiplyTests
+{
+    [Theory]
+    [InlineData(0, 2, 0)]
+    [InlineData(5, 6, 30)]
+    [InlineData(-5, 6, -30)]
+    [InlineData(-5, -6, 30)]
+
+    public void Multiply_GivenTwoNumbers_ShouldReturnTheCorrectMultiplication (int a, int b, int expected)
+    {
+        //Act
+        var result = CustomMath.Multiply(a, b);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
