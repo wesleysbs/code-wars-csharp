02a433c1b365c7aa369dbf6bda5a159cb0e36089
Wesley <wesleysbs@hotmail.com>
2026-08-26 22:50:39 -0300

    feat: add RockPaperScissors challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/RockPaperScissorsTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class RockPaperScissorsTests
+{
+    [Theory]
+    [InlineData("paper", "paper", "Draw!")]
+    [InlineData("scissors", "paper", "Player 1 won!")]
+    [InlineData("scissors", "rock", "Player 2 won!")]
+
+    public void RockPaperScissors_GivenTwoStrings_ShouldReturnCorrectResult(string p1, string p2, string expected)
+    {
+        //Act
+        var result = RockPaperScissors.Rps(p1, p2);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
