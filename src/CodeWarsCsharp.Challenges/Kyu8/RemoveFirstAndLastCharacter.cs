24bcae6dbb606bae790d32c60a3c35456609776c
Wesley <wesleysbs@hotmail.com>
2026-08-26 22:19:44 -0300

    feat: add RemoveFirstAndLastCharacter challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/RemoveFirstAndLastCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+public class RemoveFirstAndLastCharacter
+{
+    public static string RemoveChar(string s)
+    {
+        return s.Substring(1, s.Length - 2);
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/RemoveFirstAndLastCharacterTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class RemoveFirstAndLastCharacterTests
+{
+    [Theory]
+    [InlineData("eloquent", "loquen")]
+    [InlineData("ab", "")]
+    [InlineData("xyz", "y")]
+
+    public void RemoveChar_GivenAString_ShouldRemoveFirstAndLastCharacters (string s, string expected)
+    {
+        //Act
+        var result = RemoveFirstAndLastCharacter.RemoveChar(s);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
