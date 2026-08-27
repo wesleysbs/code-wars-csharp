c7495b6909d50dd22b8c1a6d96369536b641ba1f
Wesley <wesleysbs@hotmail.com>
2026-08-26 22:51:13 -0300

    feat: add RockPaperScissors challenge and tests (#97)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/RockPaperScissors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class RockPaperScissors
+{
+    public static string Rps(string p1, string p2)
+    {
+        if (p1 == p2)
+        {
+            return "Draw!";
+        }
+
+        else if (p1 == "rock" && p2 == "scissors" || p1 == "scissors" && p2 == "paper" || p1 == "paper" && p2 == "rock")
+        {
+            return "Player 1 won!";
+        }
+
+        else
+        {
+            return "Player 2 won!";
+        }
+    }
+}
