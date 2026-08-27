95fdb00970ab8a1c9b28e62ac973fd4b674b3f13
Wesley <wesleysbs@hotmail.com>
2026-08-27 20:51:39 -0300

    feat: add DoIGetABonus challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/DoIGetABonus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class DoIGetABonus
+{
+    public static string BonusTime(int salary, bool bonus)
+    {
+        return bonus ? $"${salary * 10}" : $"${salary}";
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/DoIGetABonusTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class DoIGetABonusTests
+{
+    [Theory]
+    [InlineData(10000, true, "$100000")]
+    [InlineData(78, false, "$78")]
+    [InlineData(2, true, "$20")]
+    [InlineData(60000, false, "$60000")]
+
+    public void BonusTime_GivenASalaryAndBonus_ShouldReturnTheCorrectSalary(int salary, bool bonus, string expected)
+    {
+        //Act
+        var result = DoIGetABonus.BonusTime(salary, bonus);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
