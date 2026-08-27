3f008cc95c1193fdbb492f5a3e8f0c26d26c01b4
Wesley <wesleysbs@hotmail.com>
2026-08-26 22:32:12 -0300

    feat: add OppositeNumber challenge and tests (#96)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/OppositeNumberTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class OppositeNumberTests
+{
+    [Theory]
+    [InlineData(1, -1)]
+    [InlineData(25, -25)]
+    [InlineData(0, 0)]
+    [InlineData(-10, 10)]
+
+    public void Opposite_GivenANumber_ShouldReturnItsOpposite(int number, int expected)
+    {
+        //Act
+        var result = OppositeNumber.Opposite(number);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
