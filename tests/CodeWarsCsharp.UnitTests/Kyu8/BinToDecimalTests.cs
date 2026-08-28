fd2a3429a7a672771b337316c63a697d27409f99
Wesley <wesleysbs@hotmail.com>
2026-08-27 21:06:59 -0300

    feat: add BinToDecimal challenge and tests (#99)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/BinToDecimalTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class BinToDecimalTests
+{
+    [Theory]
+    [InlineData("0", 0)]
+    [InlineData("1", 1)]
+    [InlineData("10", 2)]
+    [InlineData("110", 6)]
+
+    public void BinToDec_GivenABinaryString_ShouldReturnTheDecimalNumber(string s, int expected)
+    {
+        //Act
+        var result = BinToDecimal.BinToDec(s);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
