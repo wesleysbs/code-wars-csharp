bd2d0495de3db1b69e83b427cf892faba4153f47
Wesley <wesleysbs@hotmail.com>
2026-08-27 21:06:21 -0300

    feat: add BinToDecimal challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/BinToDecimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class BinToDecimal
+{
+    public static int BinToDec(string s)
+    {
+        return Convert.ToInt32(s, 2);
+    }
+}
