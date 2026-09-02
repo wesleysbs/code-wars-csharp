9941daee7ef2f80a3133d2aa7d0723b5a650b5f1
Wesley <wesleysbs@hotmail.com>
2026-09-02 20:33:17 -0300

    feat: add SentenceSmash challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/SentenceSmash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class SentenceSmash
+{
+    public static string Smash(string[] words)
+    {
+        string result = "";
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            result += words[i];
+
+            if (i != words.Length - 1)
+            {
+                result += " ";
+            }
+        }
+        return result;
+    }
+}
---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/SentenceSmashTests.cs
@@ -0,0 +1,51 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class SentenceSmashTests
+{
+    [Fact]
+    public void Smash_GivenAnArray_ShouldReturnSentence()
+    {
+        //Arrange
+        var inputValue = new string[] { "hello", "world", "this", "is", "great" };
+        var expectedResult = "hello world this is great";
+
+        //Act
+        var result = SentenceSmash.Smash(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void Smash_GivenTwoWords_ShouldReturnSentence()
+    {
+        //Arrange
+        var inputValue = new string[] { "hello", "world" };
+        var expectedResult = "hello world";
+
+        //Act
+        var result = SentenceSmash.Smash(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void Smash_GivenOneWord_ShouldReturnTheSameWord()
+    {
+        //Arrange
+        var inputValue = new string[] { "hello" };
+        var expectedResult = "hello";
+
+        //Act
+        var result = SentenceSmash.Smash(inputValue);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
