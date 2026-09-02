fb882de8d5c25d8e2fc0fdbfa461ccb0c88c4706
Wesley <wesleysbs@hotmail.com>
2026-09-01 21:21:44 -0300

    feat: add EvenOrOdd challenge and tests (#100)

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/EvenOrOdd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class EvenOrOdd
+{
+    public static string CheckEvenOrOdd(int number)
+    {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+}
