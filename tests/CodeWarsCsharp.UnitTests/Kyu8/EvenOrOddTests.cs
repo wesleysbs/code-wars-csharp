1271c5146ac0f122827dd78599dd03f8de1d5a61
Wesley <wesleysbs@hotmail.com>
2026-09-01 21:20:53 -0300

    feat: add EvenOrOdd challenge and tests

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/EvenOrOddTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class EvenOrOddTests
+{
+    [Theory]
+    [InlineData(1, "Odd")]
+    [InlineData(4, "Even")]
+    [InlineData(37, "Odd")]
+    [InlineData(0, "Even")]
+    [InlineData(-5, "Odd")]
+
+    public void CheckEvenOrOdd_GivenANumber_ShouldReturnCorrectParity(int number, string expected)
+    {
+        //Act
+        var result = EvenOrOdd.CheckEvenOrOdd(number);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
