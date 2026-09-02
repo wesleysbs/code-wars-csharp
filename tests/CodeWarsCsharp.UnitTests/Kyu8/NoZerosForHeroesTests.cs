9d9ce3b5d74fe0a845d05a600e5df76c0d56c348
Wesley <wesleysbs@hotmail.com>
2026-09-01 21:49:54 -0300

    feat: add NoZerosForHeroes challenge and tests (#101)

---
+++ tests/CodeWarsCsharp.UnitTests/Kyu8/NoZerosForHeroesTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWarsCsharp.Challenges.Kyu8;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.UnitTests.Kyu8;
+
+public class NoZerosForHeroesTests
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(1450, 145)]
+    [InlineData(960000, 96)]
+    [InlineData(1050, 105)]
+    [InlineData(-1050, -105)]
+
+    public void NoBoringZeros_GivenANumber_ShouldRemoveTrailingZeros(int number, int expected)
+    {
+        //Act
+        var result = NoZerosForHeroes.NoBoringZeros(number);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+}
