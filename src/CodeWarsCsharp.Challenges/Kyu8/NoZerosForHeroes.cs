719eae906c23ee46202c66abe49a189171465b6c
Wesley <wesleysbs@hotmail.com>
2026-09-01 21:47:36 -0300

    feat: add NoZerosForHeroes challenge and tests

---
+++ src/CodeWarsCsharp.Challenges/Kyu8/NoZerosForHeroes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsCsharp.Challenges.Kyu8;
+
+public class NoZerosForHeroes
+{
+    public static int NoBoringZeros(int n)
+    {
+        while (n % 10 == 0 && n != 0)
+        {
+            n /= 10;
+        }
+        return n;
+    }
+}
